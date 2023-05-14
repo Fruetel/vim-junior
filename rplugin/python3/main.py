e0f73bb0c9a8889e106312862a1ccc9dcf00bdac
Thomas Fruetel <github@fruetel.de>
2023-05-14 21:02:53 +0200

    Improve error handling

--- rplugin/python3/main.py
+++ rplugin/python3/main.py
@@ -11,21 +11,21 @@ class JuniorPlugin(object):
         # containing a line like this:
         # OPENAI_API_KEY=your-api-key-here
         with open(os.path.expanduser('~/.junior'), 'r') as f:
-            openai.api_key = f.readline().split('=')[1]
+            openai.api_key = f.readline().split('=')[1].strip()
 
     @neovim.command('Chat', nargs='*')
     def chat(self, args):
-        # You can replace the following lines with the actual API call
-        # Make a GET request
-        # response = requests.get('http://example.com')
-
         prompt = ' '.join(args)
 
+        # Initialize response to an empty string
+        response = ''
+
         try:
-            completion = openai.Completion.create(model="text-davinci-003", prompt=prompt)
-            response = completion.choices[0].text
+            completion = openai.Completion.create(model="text-davinci-003", prompt=prompt, max_tokens=150)
+            response = completion.choices[0].text.strip()
         except Exception as e:
-            self.nvim.out_write(f'Error: {e}\n')
+            response = f'Error: {e}'
 
         # Feed the query back to the user
-        self.nvim.out_write(response)
+        self.nvim.out_write(response + '\n')
+
